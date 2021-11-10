5be6493731612eae997bc0fd7e08eef375cc468e
jubaer-hossen <jobaer1618@gmail.com>
2021-11-10 22:41:44 +0600

    added about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,14 +5,14 @@ const About = () => {
     return (
         <div className="about d-flex align-items-center justify-content-center">
             <div className="text-bg-color text-white text-center">
-                <h1>We provide the best courier services for you! </h1>
-                <h3>You can simply click and order any service.</h3>
+                <h1>We provide the best cameras for you! </h1>
+                <h3>You can simply click and order any camera or cameras.</h3>
                 <p>
-                    You can simply contact the courier office. and know about
-                    your service
+                    You can simply contact the shop office. and know about your
+                    service / camera
                 </p>
-                <p>You can visit courier office and purchase your service</p>
-                <p>Also, you can get services for emergency courier.</p>
+                <p> You can visit Our shop and purchase your Dream camera.</p>
+                <p> Also, you can get services for emergency Camera.</p>
             </div>
         </div>
     );
